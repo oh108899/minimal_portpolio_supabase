5ed4031db0e34aea3ebe607912fda6019d1e7c6b
oh108899 <oh108899@gmail.com>
2026-01-23 14:32:23 +0900

    feat:layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-// import "./globals.css";
 
+import Image from "next/image";
 
 
 export const metadata = {
@@ -11,7 +11,50 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <header>
+          <h1 className="logo"><a href="">Minimal Portfolio Theme</a></h1>
+          <nav>
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="portfolio.html">Portfolio</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+        <hr />
+        <main className="content">
+          {children}
+        </main>
+        <footer>
+          <div className="quote_area">
+            <h3 className="heading6">Need a quote?</h3>
+            <p>
+              Please use the form inside the contact page. Make sure you include some personal information as well as
+              your project description and available budget.
+            </p>
+            <p><a href="">Get a free quote &rarr;</a></p>
+          </div>
+          <div className="copyright">
+            <h3 className="heading6">Just wanna say hi?</h3>
+            <p>
+              You can call me, email me directly or connect with me through my social networks.
+            </p>
+            <p>
+              (+40) 744122222<br />
+              <a href="mailto:hello@adipurdila.com">hello@adipurdila.com</a>
+            </p>
+            <ul className="social_links">
+              <li><a href=""><Image src="/images/twitter.png" alt="twitter" width={32} height={32} /></a></li>
+              <li><a href=""><Image src="/images/facebook.png" alt="facebook" width={32} height={32} /></a></li>
+              <li><a href=""><Image src="/images/dribble.png" alt="dribble" width={32} height={32} /></a></li>
+            </ul>
+            <hr />
+            <p>
+              (c) Copyright 2020. Portfolio theme by alikerock.
+            </p>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -7,19 +7,43 @@ export default async function Home() {
 
   return (
     <>
-      <h1>
-        Welcome to the Minimal Portfolio Website.
-      </h1>
-      <ul>
-        {
-          projects.map(p=>{return(
-            <li key={p.id}>
-              <h3>{p.title}</h3>
-              <p>{p.content}</p>
-            </li>
-          )})
-        }
-      </ul>
+      <div className="container latest_portfolio">
+        <div className="row intro">
+          <div className="col-md-4">
+            <div className="contents shadow">
+              <h2 className="heading2">I&apos;m alikerock</h2>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="contents shadow">
+              <h2 className="heading2">I create super awesome stuff</h2>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="contents shadow">
+              <h2 className="heading2">I&apos;m available for freelance projects</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row list">
+          {/* <div className="col-md-4">
+            <div className="contents shadow">
+              <img src="images/latest_portfolio_01.jpg" alt="latest_portfolio_01" />
+                <div className="hover_contents">
+                  <div className="list_info">
+                    <h3><a href="">Project Title</a> <img src="images/portfolio_list_arrow.png"
+                      alt="list arrow" /></h3>
+                    <p><a href="">Click to see project</a></p>
+                  </div>
+                </div>
+            </div>
+          </div> */}
+
+        </div>
+        <p className="porfolio_readmore">
+          <a href="" className="primary-btn">See my full portfolio</a>
+        </p>
+      </div>
     </>
   );
 }
